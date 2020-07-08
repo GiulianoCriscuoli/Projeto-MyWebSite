cfec51ef2b8764d52e35a2b1e42f795a10bb85a1
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-07-07 23:57:14 -0300

    Acrescentando texto no modal do banner

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+// variáveis para o banner rotativo automático
+
 let time =4000, indexImage = 0,
     images = document.querySelectorAll(".slide img"),
     maxImages = images.length;
 
+// variáveis de mudar o banner clicando
+
+let pointer = document.querySelectorAll(".pointer .point"),
+    position = 0;
+
+
+
+
+    // rotaciona o banner
 
 function rotationImage() {
 
@@ -20,6 +31,7 @@ function rotationImage() {
 
 }
 
+// rotaciona a cada intervalo
 
 function rotationStart() {
 
@@ -34,3 +46,21 @@ function rotationStart() {
 }
 
 window.addEventListener("load", rotationStart);
+
+
+function choicePoint() {
+
+    pointer.forEach(point => {
+
+        point.addEventListener("click", () => {
+
+        
+
+          
+        });
+
+
+    });
+
+
+}
